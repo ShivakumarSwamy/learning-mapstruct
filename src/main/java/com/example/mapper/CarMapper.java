d82b8d1d0c0505022dbb6eaf6031fd6a3b9dd759
Shivakumar Swamy <shiva.swamy@logmein.com>
2020-12-01 21:04:27 +0100

    explict mapping

--- src/main/java/com/example/mapper/CarMapper.java
+++ src/main/java/com/example/mapper/CarMapper.java
@@ -1,11 +1,14 @@
 package com.example.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
 public interface CarMapper {
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(source = "name", target = "name")
     @Mapping(source = "make", target = "manufacturer")
     @Mapping(source = "numberOfSeats", target = "seatCount")
     CarDto carToCarDto(Car car);
